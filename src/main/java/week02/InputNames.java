2025e04e80053d02425cc0b06b1d33e52e2e636e
Lipek <lipek71@gmail.com>
2020-11-05 10:39:28 +0100

    Literálok és lokális változók

---
+++ src/main/java/week02/InputNames.java
@@ -0,0 +1,27 @@
+package week02;
+
+import java.util.Scanner;
+
+public class InputNames {
+    public static void main(String[] args) {
+
+        String [] names = new String [5];
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i <  names.length; i++) {
+
+            int j = i + 1;
+            System.out.println("Kérem a " + j + ". nevet:");
+            names[i] = scanner.nextLine();
+        }
+
+        for (int i = 0; i <  names.length; i++) {
+
+            int j = i + 1;
+            System.out.println("Az " + j + ". név: " + names[i]);
+
+        }
+
+    }
+}
