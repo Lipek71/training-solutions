c0b9ea42e4bccb9dc61304f6a3b17407f4864665
Lipek <lipek71@gmail.com>
2020-11-15 21:24:45 +0100

    Gentleman.java

---
+++ src/main/java/finalmodifier/Gentleman.java
@@ -0,0 +1,13 @@
+package finalmodifier;
+
+public class Gentleman {
+    final String MESSAGE_PREFIX = "Hello";
+    String sayHello(String name){
+        return MESSAGE_PREFIX + " " + name;
+    }
+
+    public static void main(String[] args) {
+        Gentleman gentleman = new Gentleman();
+        System.out.println(gentleman.sayHello("Laci"));
+    }
+}
