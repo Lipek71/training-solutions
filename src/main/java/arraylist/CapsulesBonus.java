8cdc5f892b8e003d42c8f48d01eb13c918c32674
Lipek <lipek71@gmail.com>
2020-11-15 19:31:12 +0100

    CapsulesBonus.java

---
+++ src/main/java/arraylist/CapsulesBonus.java
@@ -0,0 +1,54 @@
+package arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CapsulesBonus {
+    private List<String> capsules = new ArrayList<>();
+
+    void addLast(String color) {
+        capsules.add(capsules.size(), color);
+    }
+
+    void addFirst(String color) {
+        capsules.add(0, color);
+    }
+
+    void removeFirst() {
+        capsules.remove(0);
+    }
+
+    void removeLast() {
+        capsules.remove(capsules.size() - 1);
+    }
+
+    List<String> getColors() {
+        return capsules;
+
+    }
+
+    public static void main(String[] args) {
+        Capsules capsules = new Capsules();
+        List<String> capsules2 = new ArrayList<>();
+        capsules.addFirst("piros");
+        System.out.println(capsules.getColors());
+        capsules.addFirst("kék");
+        System.out.println(capsules.getColors());
+        capsules.addLast("zöld");
+        System.out.println(capsules.getColors());
+        capsules.addFirst("lila");
+        System.out.println(capsules.getColors());
+        capsules.addLast("sárga");
+        System.out.println(capsules.getColors());
+
+        System.out.println();
+        capsules2 = capsules.getColors();
+        System.out.println(capsules2);
+        capsules2.clear();
+        System.out.println(capsules2);
+        System.out.println();
+        System.out.println(capsules.getColors());
+
+    }
+
+}
