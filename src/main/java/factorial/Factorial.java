75da7be8ef7e7732d13b708ca484300b263958d3
Lipek <lipek71@gmail.com>
2020-11-18 22:52:24 +0100

    Rekurzió

---
+++ src/main/java/factorial/Factorial.java
@@ -0,0 +1,20 @@
+package factorial;
+
+public class Factorial {
+    public static long factorial(int factorial) {
+        if (factorial > 1) {
+            long solution = factorial(factorial - 1);
+            return factorial * solution;
+        } else {
+            return 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        for(int i=0; i < 11; i++){
+            System.out.println(i + " faktoriálisa: " + factorial(i));
+            System.out.println();
+        }
+
+    }
+}
