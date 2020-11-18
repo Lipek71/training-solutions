21211a174bb63704138087a9f1714264ee6a79a9
Lipek <lipek71@gmail.com>
2020-11-18 10:39:45 +0100

    Week04 órai feladat

---
+++ src/main/java/week04/NumberFinder.java
@@ -0,0 +1,30 @@
+package week04;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberFinder {
+
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        int number=rnd.nextInt(99) +1;
+        Scanner scanner = new Scanner(System.in);
+        int numberFind;
+        System.out.println(number);
+        for(int i=0; i < 6; i++){
+            System.out.println("Kérek egy számot: ");
+            numberFind = scanner.nextInt();
+            scanner.nextLine();
+            if(number > numberFind){
+                System.out.println("Nagyobbra gondoltam!");
+            } else if(number < numberFind){
+                System.out.println("Kisebbre gondoltam!");
+            } else if(number == numberFind){
+                System.out.println("Ügyes vagy kitaláltad!");
+                break;
+            }
+        }
+        System.out.println("Sajnos nem találtad ki: " + number + " volt a szám");
+
+    }
+}
