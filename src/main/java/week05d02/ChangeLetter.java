45c1c926e112dc3bdd96d9e4d7379f2db91bd375
Lipek <lipek71@gmail.com>
2020-11-24 18:34:14 +0100

    Órai feladat primitív megoldása, primitív megoldás javítása és az optimális vagy ahhoz közeli megoldás

--- src/main/java/week05d02/ChangeLetter.java
+++ src/main/java/week05d02/ChangeLetter.java
@@ -1,13 +1,39 @@
 package week05d02;
 
 public class ChangeLetter {
-    public String changeVowels(String str){
-        String strBack ="";
-        strBack = str.toLowerCase().replace('a','*');
-        strBack = strBack.toLowerCase().replace('e','*');
-        strBack = strBack.toLowerCase().replace('i','*');
-        strBack = strBack.toLowerCase().replace('o','*');
-        strBack = strBack.toLowerCase().replace('u','*');
+    private static final String VOWELS = "aeiou";
+    private static final String REPLACE_CHAR = "*";
+
+    public String changeVowels(String str) {
+        String strBack = "";
+        strBack = str.toLowerCase().replace('a', '*');
+        strBack = strBack.toLowerCase().replace('e', '*');
+        strBack = strBack.toLowerCase().replace('i', '*');
+        strBack = strBack.toLowerCase().replace('o', '*');
+        strBack = strBack.toLowerCase().replace('u', '*');
         return strBack;
+    }
+
+    public String changeVowelsVer2(String str){
+        Character[] vowels = new Character[] {'a','e','i','o','u'};
+        final char REPLACE_CHAR = '*';
+        String strBack = str;
+        for(int i= 0; i < vowels.length; i++){
+            strBack = strBack.toLowerCase().replace(vowels[i], REPLACE_CHAR);
+        }
+        return strBack;
+    }
+
+    public String changeVowelsOptimal(String str) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            String actualChar = str.substring(i, i + 1);
+            if (VOWELS.contains(actualChar)) {
+                builder.append(REPLACE_CHAR);
+            } else {
+                builder.append(actualChar);
+            }
+        }
+        return builder.toString();
     }
 }
--- src/test/java/week05d02/ChangeLetterTest.java
+++ src/test/java/week05d02/ChangeLetterTest.java
@@ -6,14 +6,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ChangeLetterTest {
     @Test
-    public void changeVowelsTest(){
+    public void changeVowelsTest() {
         ChangeLetter changeLetter = new ChangeLetter();
         assertEquals("l*l", changeLetter.changeVowels("lol"));
     }
+
     @Test
-    public void changeVowelsTest2(){
+    public void changeVowelsTest2() {
         ChangeLetter changeLetter = new ChangeLetter();
         assertEquals("l*l*l*l*l*l", changeLetter.changeVowels("lalelilolul"));
+    }
+
+    @Test
+    public void changeVowelsTest3() {
+        assertEquals("*rv*zt*r*f*r*g*p", new ChangeLetter().changeVowelsVer2("arvizturofurogep"));
+    }
+
+    @Test
+    public void changeVowelsTest4() {
+        assertEquals("*rv*zt*r*f*r*g*p", new ChangeLetter().changeVowelsOptimal("arvizturofurogep"));
     }
 
 }
