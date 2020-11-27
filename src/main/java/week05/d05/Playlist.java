4bdc1b6b7230ef9eaaaa552d30b5db64e433428b
Lipek <lipek71@gmail.com>
2020-11-27 11:14:22 +0100

    Órai feladat

--- src/main/java/week05/d05/Playlist.java
+++ src/main/java/week05/d05/Playlist.java
@@ -28,24 +28,4 @@ public class Playlist {
                 '}';
     }
 
-    public static void main(String[] args) {
-        Song song1 = new Song("Első", 150, "Chaerlie");
-        System.out.println(song1);
-        Song song2 = new Song("Második", 200, "Eminem");
-        Song song3 = new Song("Harmadik", 250, "U2");
-        Song song4 = new Song("Utolsó", 125, "Kispál");
-
-        List<Song> songs = new ArrayList<Song>();
-        songs.add(song1);
-        songs.add(song2);
-        songs.add(song3);
-        songs.add(song4);
-
-        System.out.println(songs);
-
-        Playlist playlist = new Playlist(songs);
-        System.out.println(playlist.findByLengthGreaterThan(2));
-
-
     }
-}
--- src/test/java/week05/d05/PlaylistTest.java
+++ src/test/java/week05/d05/PlaylistTest.java
@@ -1,4 +1,30 @@
 package week05.d05;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PlaylistTest {
+
+
+    @Test
+    public void test1Playlist() {
+        Song song1 = new Song("Első", 150, "Chaerlie");
+        Song song2 = new Song("Második", 200, "Eminem");
+        Song song3 = new Song("Harmadik", 250, "U2");
+        Song song4 = new Song("Utolsó", 125, "Kispál");
+
+        List<Song> songs = new ArrayList<Song>();
+        songs.add(song1);
+        songs.add(song2);
+        songs.add(song3);
+        songs.add(song4);
+
+        Playlist playlist = new Playlist(songs);
+
+        assertEquals(2,playlist.findByLengthGreaterThan(2).size() );
+    }
 }
