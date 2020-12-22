f918d4f925d82a4608f39df4fc81ec2576a6ce1b
Lipek <lipek71@gmail.com>
2020-12-22 11:21:38 +0100

    Órai

---
+++ src/test/java/week09/d02/FibCalculatorTest.java
@@ -0,0 +1,20 @@
+package week09.d02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FibCalculatorTest {
+
+    @Test
+    void test1SumEvents() {
+        FibCalculator fibCalculator = new FibCalculator();
+        assertEquals(10, fibCalculator.sumEvents(10));
+    }
+
+    @Test
+    void Test2Sumvents() {
+        FibCalculator fibCalculator = new FibCalculator();
+        assertEquals(44, fibCalculator.sumEvents(35));
+    }
+}
