8504ba49dc75ba72f02164622e99918cde78d0b1
Lipek <lipek71@gmail.com>
2020-12-22 19:55:58 +0100

    Órai

--- src/main/java/week09/d02/FibCalculator.java
+++ src/main/java/week09/d02/FibCalculator.java
@@ -1,6 +1,10 @@
 package week09.d02;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FibCalculator {
+    List<String> stringList = new ArrayList<>();
     public long fib(int n) {
         if (n <= 1) {
             return n;
@@ -13,11 +17,12 @@ public class FibCalculator {
         long fibNumber = 0;
         int count = 0;
         while (fibNumber <= bound) {
-            fibNumber = fib(count);
-            if (fibNumber % 2 == 0) {
+           if (fibNumber % 2 == 0) {
                 sumEvensSmallThanBound += fibNumber;
             }
             count++;
+            fibNumber = fib(count);
+
         }
         return sumEvensSmallThanBound;
     }
@@ -26,18 +31,20 @@ public class FibCalculator {
         int sumEvensSmallThanBound = 0;
         long fibNumber = 0;
         int count = 0;
-        while (fibNumber <= bound) {
+        while (sumEvensSmallThanBound <= bound) {
             if (fibNumber % 2 == 0) {
                 sumEvensSmallThanBound += fibNumber;
             }
-            fibNumber = fib(count);
             count++;
+            fibNumber = fib(count);
         }
         return sumEvensSmallThanBound;
     }
 
     public static void main(String[] args) {
         FibCalculator fibCalculator = new FibCalculator();
-        System.out.println(fibCalculator.sumEvens(35));
+        int bound = 7;
+        System.out.println(fibCalculator.sumEvens(bound));
+        System.out.println(fibCalculator.sumEvens2(bound));
     }
 }
