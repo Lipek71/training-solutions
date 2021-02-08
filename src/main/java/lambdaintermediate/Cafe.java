31957629d39f8b51833c85c228bea2af92d195a1
Lipek <lipek71@gmail.com>
2021-02-08 20:17:57 +0100

    Gyakorlás

--- src/main/java/lambdaintermediate/Cafe.java
+++ src/main/java/lambdaintermediate/Cafe.java
@@ -1,9 +1,13 @@
 package lambdaintermediate;
 
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -15,30 +19,39 @@ public class Cafe {
         this.coffeeOrderList = coffeeOrderList;
     }
 
-    public long getTotalIncome(){
-
-        return 123;
+    public BigDecimal getTotalIncome(){
+        return coffeeOrderList.stream()
+                .flatMap(cOL -> cOL.getCoffeeList().stream())
+                .map(gCL -> gCL.getPrice())
+                .reduce(BigDecimal.ZERO, (a, b) -> a.add(b), (x, y) -> x.add(y)).setScale(2, RoundingMode.HALF_UP);
     }
 
-    public long getTotalIncome(LocalDate date){
-
-        return 124;
+    public BigDecimal getTotalIncome(LocalDate date){
+        return coffeeOrderList.stream()
+                .filter(cOL -> cOL.getDateTime().toLocalDate().equals(date))
+                .flatMap(cOL -> cOL.getCoffeeList().stream())
+                .map(gCL -> gCL.getPrice())
+                .reduce(BigDecimal.ZERO, (a, b) -> a.add(b), (x, y) -> x.add(y)).setScale(2, RoundingMode.HALF_UP);
     }
 
-    public int getNumberOfCoffee(CoffeeType type){
-
-        return 125;
+    public long getNumberOfCoffee(CoffeeType type){
+        return coffeeOrderList.stream()
+                .flatMap(cOL -> cOL.getCoffeeList().stream())
+                .filter(gCL -> gCL.getType().equals(type))
+                .count();
     }
 
     public List<CoffeeOrder> getOrdersAfter(LocalDateTime from){
-        List<CoffeeOrder> ordersAfter = new ArrayList<>();
-
-        return ordersAfter;
+        return  coffeeOrderList.stream()
+                .filter(cOL -> cOL.getDateTime().isAfter(from))
+                .collect(Collectors.toList());
     }
 
     public List<CoffeeOrder>  getFirstFiveOrder(LocalDate date){
-        List<CoffeeOrder> firstFiveCoffeeType = new ArrayList<>();
-
-        return firstFiveCoffeeType;
+        return  coffeeOrderList.stream()
+                .filter(cOL -> cOL.getDateTime().toLocalDate().equals(date))
+                .sorted((cOL1, cOL2) -> cOL1.getDateTime().compareTo(cOL2.getDateTime()))
+                .limit(5)
+                .collect(Collectors.toList());
     }
 }
