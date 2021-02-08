3e3a65a4ca23638fd5299480e477e8977620b1ed
Lipek <lipek71@gmail.com>
2021-02-08 01:30:17 +0100

    Gyakorlás

---
+++ src/main/java/lambdaprimitives/Product.java
@@ -0,0 +1,13 @@
+package lambdaprimitives;
+
+public class Product {
+    private final String name;
+    private final int price;
+    private final int pieces;
+
+    public Product(String name, int price, int pieces) {
+        this.name = name;
+        this.price = price;
+        this.pieces = pieces;
+    }
+}
