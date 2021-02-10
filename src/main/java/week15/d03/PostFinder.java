9c63f2c7dc2a61a10a9f7f3b9212b8f7b0e647b2
Lipek <lipek71@gmail.com>
2021-02-10 10:16:11 +0100

    Órai

---
+++ src/main/java/week15/d03/PostFinder.java
@@ -0,0 +1,23 @@
+package week15.d03;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PostFinder {
+    private List<Post> postList;
+
+    public PostFinder(List<Post> postList) {
+        this.postList = postList;
+    }
+
+    public List<Post> findPostsFor(String user){
+        return  postList.stream().filter(pL -> pL.getPublishedAt().isBefore(LocalDate.now())
+                || (pL.getPublishedAt().isAfter(LocalDate.now())
+                && pL.getOwner().equals(user)
+                && pL.getContent().length() > 0
+                && pL.getTitle().length() > 0
+                && (pL.getDeletedAt().equals(null) || pL.getDeletedAt().isAfter(LocalDate.now()))))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/week15/d03/PostFinderTest.java
@@ -0,0 +1,12 @@
+package week15.d03;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostFinderTest {
+
+    @Test
+    void findPostsFor() {
+    }
+}
